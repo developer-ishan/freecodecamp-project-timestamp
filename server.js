b4e91f9caf51d9965019d87e6373fb9940044459
developer-ishan <62565307+developer-ishan@users.noreply.github.com>
2021-09-22 21:51:08 +0000

    Test 1

--- server.js
+++ server.js
@@ -24,6 +24,35 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function (req, res) {
+  const {date} = req.params;
+  let parsed_if_utc = new Date(date);
+  let parsed_if_unix = new Date(date/1000);
+
+  console.log(parsed_if_utc, Object.prototype.toString.call(parsed_if_utc));
+  console.log(parsed_if_unix, Object.prototype.toString.call(parsed_if_unix));
+  
+  let parsed = null;
+  if(parsed_if_utc.toString() !== 'Invalid Date'){
+    parsed = parsed_if_utc;
+  } else if(parsed_if_unix.toString() !== 'Invalid Date'){
+    parsed = parsed_if_unix;
+  } else{
+    return res.json({ error : "Invalid Date" });
+  }
+  return res.json({
+    unix: parsed.getTime() / 1000,
+    utc: parsed.toUTCString()
+  })
+});
+
+app.get("/api", function (req, res) {
+  const cur_date = new Date();
+  return res.json({
+    unix: cur_date.getTime(),
+    utc: cur_date.toUTCString()
+  })
+});
 
 
 // listen for requests :)
